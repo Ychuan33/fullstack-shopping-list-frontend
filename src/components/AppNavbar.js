b5990af8ba90051b5dae275556797c0e7bfb1c2a
Ychuan33 <felixyan2011@gmail.com>
2019-04-10 19:05:52 -0400

    first commit

---
+++ src/components/AppNavbar.js
@@ -0,0 +1,52 @@
+import React, { Component } from "react";
+import {
+  Collapse,
+  Navbar,
+  NavbarToggler,
+  NavbarBrand,
+  Nav,
+  NavItem,
+  NavLink,
+  Container
+} from "reactstrap";
+
+class AppNavbar extends Component {
+  state = {
+    isOpen: false
+  };
+
+  toggle = () => {
+    this.setState({
+      isOpen: !this.state.isOpen
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <Navbar color="dark" dark expand="sm" className="mb-5">
+          <Container>
+            <NavbarBrand href="/">ShoppingList</NavbarBrand>
+            <NavbarToggler onClick={this.toggle} />
+            <Collapse isOpen={this.state.isOpen} navbar>
+              <Nav className="ml-auto" navbar>
+                <NavItem>
+                  <span
+                    className="d-inline-block"
+                    tabIndex="3"
+                    data-toggle="tooltip"
+                    title="Check the code!"
+                  >
+                    <NavLink href="/">Github</NavLink>
+                  </span>
+                </NavItem>
+              </Nav>
+            </Collapse>
+          </Container>
+        </Navbar>
+      </div>
+    );
+  }
+}
+
+export default AppNavbar;
